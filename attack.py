5704cf7746393b689ce0ccbdc5a4104bce9174b7
jaysarva <jsarva@waabi.ai>
2023-04-09 22:08:34 +0000

    Debug attack

--- attack.py
+++ attack.py
@@ -4,14 +4,15 @@ import numpy
 def hotflip(averaged_gradient, embedding_matrix, adv_token_ids, num_candidates=1):
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    embedding_adv = torch.nn.functional.embedding(torch.IntTensor(adv_token_ids).to(device), embedding_matrix).to(device).detach()
-
-    grad_embedding = torch.einsum("ij,kj->ik", (averaged_gradient, embedding_matrix)).to(device)
+    embedding_adv = torch.nn.functional.embedding(torch.IntTensor(adv_token_ids).to(device), embedding_matrix).to(device).detach().unsqueeze(0)
+    averaged_gradient = averaged_gradient.unsqueeze(0)
+    grad_embedding = torch.einsum("bij,kj->bik", (averaged_gradient, embedding_matrix)).to(device)
     # this is assuming average gradient is squeezed and 2 dims
     # can consider adding dim 0 if makes computation simpler elsewhere
     if num_candidates > 1:
-        elts, best_k_ids = torch.topk(grad_embedding, num_candidates, dim=1)
+        elts, best_k_ids = torch.topk(grad_embedding, num_candidates, dim=2)
+        #print(best_k_ids)
         return best_k_ids.detach().to(device).cpu().numpy()[0]
-    _, best_at_step = grad_embedding.max(1)
+    _, best_at_step = grad_embedding.max(2)
     return best_at_step[0].detach().to(device).cpu().numpy()
     
